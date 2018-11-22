646c185f1ba0f9740eecdc168da70b0ef6f8ce92
Munish Rana <sam786rana786@gmail.com>
2018-11-22 21:45:33 +0530

    Face Recognition app created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Clarifai from 'clarifai';
+import Navigation from './Components/Navigation/navigation';
+import SignIn from './Components/SignIn/SignIn';
+import Register from './Components/Register/Register';
+import Logo from './Components/Logo/Logo';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './Components/FaceRecognition/FaceRecognition';
+import Rank from './Components/Rank/Rank';
 import './App.css';
+import Particles from 'react-particles-js';
+
+
+const app = new Clarifai.App({
+  apiKey: '1dbaf1cb14764007823f6535dfa0c9fa'
+ });
+
+const particlesOptions = {
+  particles : {
+    number: {
+      value: 200,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
 
 class App extends Component {
+  constructor()  {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+      route: 'signin',
+      isSignedIn: false
+    }
+  }
+
+  // displayFacBox = (box) => {
+  //   this.setState({box: box});
+  // }
+  calculateFaceLocation = (data) => {
+    const clarifiFace = data.outputs[0].data.regions[0].region_info.bounding_box
+    const image = document.getElementById('image');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {      
+      leftCol: clarifiFace.left_col * width,
+      topRow: clarifiFace.top_row * height,
+      rightCol: width - (clarifiFace.right_col * width),
+      bottomRow: height - (clarifiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box: box});
+  }
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    app.models
+    .predict(
+      Clarifai.FACE_DETECT_MODEL,
+      this.state.input)
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
+  }
+  onRouteChange = (route) => {
+    if( route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route});
+  }
   render() {
+    const {isSignedIn, imageUrl, route, box} = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Particles params={particlesOptions} className="particles"/>
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+        {route === 'home'
+        ?<div>
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+        <FaceRecognition box={box} imageUrl = {imageUrl}/>
+        </div>
+        :(          
+        route === 'signin'
+          ?<SignIn onRouteChange={this.onRouteChange}/>
+          :<Register onRouteChange={this.onRouteChange}/>
+        )
+        }
+        
       </div>
     );
   }
---
+++ src/Components/Logo/Logo.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+import brain from './brain.png';
+import './logo.css';
+
+const Logo = () => {
+    return (
+        <div className="ma4 mt0">            
+            <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+                <div className="Tilt-inner">
+                    <img src={brain} alt="logo" style={{padding: '20px'}} />
+                </div>
+            </Tilt> 
+        </div>
+    );
+}
+export default Logo;
